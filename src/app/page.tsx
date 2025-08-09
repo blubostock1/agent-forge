d318d75fd63e8c230be6fc006cc4c3c7e1394e39
Blu Bostock <bostock.blu.dev@gmail.com>
2025-08-09 18:54:38 +0100

    add button to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <Button variant="destructive">Click me</Button>
   );
 }
