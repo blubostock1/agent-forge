777a902441b9ba31b57fe22f8870083fea7a3cf0
Blu Bostock <bostock.blu.dev@gmail.com>
2025-08-12 12:11:52 +0100

    add home page callback

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -2,10 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 
 export const HomeView = () => {
   const {data: session} = authClient.useSession();
+  const router = useRouter();
 
   if (!session) {
     return <p>Loading...</p>;
@@ -14,7 +16,7 @@ export const HomeView = () => {
   return (
     <div className="flex flex-col p-4 gap-y-4">
         <p>Logged in as {session.user.name}</p>
-        <Button onClick={() => authClient.signOut()}>
+        <Button onClick={() => authClient.signOut({fetchOptions: {onSuccess: () => {router.push("/sign-in")}}})}>
           Sign Out
         </Button>
     </div>
